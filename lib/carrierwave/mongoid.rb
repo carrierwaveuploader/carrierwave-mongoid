a87bf404101fc28d51ac2c40e8ce88fb6dd68648
Mohak Juneja <mohakjuneja@yahoo.com>
2016-04-13 00:55:00 +0530

    add mount_uploaders

--- lib/carrierwave/mongoid.rb
+++ lib/carrierwave/mongoid.rb
@@ -4,6 +4,7 @@ require 'mongoid'
 require 'mongoid-grid_fs'
 require 'carrierwave'
 require 'carrierwave/validations/active_model'
+require 'byebug'
 
 module CarrierWave
   module Mongoid
@@ -13,7 +14,7 @@ module CarrierWave
     #
     def mount_uploader(column, uploader=nil, options={}, &block)
       field options[:mount_on] || column
-
+      
       super
 
       alias_method :read_uploader, :read_attribute
@@ -104,9 +105,126 @@ module CarrierWave
         end
       RUBY
     end
+
+    def mount_uploaders(column, uploader=nil, options={}, &block)
+      field options[:mount_on] || column, type: Array, :default => []
+
+      # super
+
+      alias_method :read_uploader, :read_attribute
+      alias_method :write_uploader, :write_attribute
+      public :read_uploader
+      public :write_uploader
+
+      include CarrierWave::Validations::ActiveModel
+
+      # validates_integrity_of  column if uploader_option(column.to_sym, :validate_integrity)
+      # validates_processing_of column if uploader_option(column.to_sym, :validate_processing)
+
+      after_save :"store_#{column}!"
+      before_save :"write_#{column}_identifier"
+      after_destroy :"remove_#{column}!"
+      before_update :"store_previous_model_for_#{column}"
+      after_save :"remove_previously_stored_#{column}"
+
+      class_eval <<-RUBY, __FILE__, __LINE__+1
+        def #{column}=(new_file)
+          column = _mounter(:#{column}).serialization_column
+
+          # mongoid won't upload a new file if there was no file previously.
+          write_uploader(column, '_old_') if self.persisted? && read_uploader(column).nil?
+
+          send(:"\#{column}_will_change!")
+          super
+        end
+
+        def remove_#{column}=(arg)
+          if ['1', true].include?(arg)
+            column = _mounter(:#{column}).serialization_column
+            send(:"\#{column}_will_change!")
+          end
+          super
+        end
+
+        def remove_#{column}!
+          super unless respond_to?(:paranoid?) && paranoid? && flagged_for_destroy?
+        end
+
+        # Overrides Mongoid's default dirty behavior to instead work more like
+        # ActiveRecord's. Mongoid doesn't deem an attribute as changed unless
+        # the new value is different than the original. Given that CarrierWave
+        # caches files before save, it's necessary to know that there's a
+        # pending change even though the attribute value itself might not
+        # reflect that yet.
+        def #{column}_changed?
+          changed_attributes.has_key?("#{column}")
+        end
+
+        # The default Mongoid attribute_will_change! method is not enough
+        # when we want to upload a new file in an existing embedded document.
+        # The custom version of that method forces the callbacks to be
+        # ran and so does the upload.
+        def #{column}_will_change!
+          changed_attributes["#{column}"] = '_new_'
+        end
+
+        def find_previous_model_for_#{column}
+          if self.embedded?
+            if self.respond_to?(:__metadata) # Mongoid >= 4.0.0.beta1
+              ancestors = [[ self.__metadata.key, self._parent ]].tap { |x| x.unshift([ x.first.last.__metadata.key, x.first.last._parent ]) while x.first.last.embedded? }
+            else # Mongoid < 4.0.0.beta1
+              ancestors = [[ self.metadata.key, self._parent ]].tap { |x| x.unshift([ x.first.last.metadata.key, x.first.last._parent ]) while x.first.last.embedded? }
+            end
+            first_parent = ancestors.first.last
+            reloaded_parent = first_parent.class.unscoped.find(first_parent.to_key.first)
+            association = ancestors.inject(reloaded_parent) { |parent,(key,ancestor)| (parent.is_a?(Array) ? parent.find(ancestor.to_key.first) : parent).send(key) }
+            association.is_a?(Array) ? association.find(to_key.first) : association
+          else
+            self.class.unscoped.for_ids(to_key).first
+          end
+        end
+
+        # def serializable_hash(options=nil)
+        #   hash = {}
+
+        #   except = options && options[:except] && Array.wrap(options[:except]).map(&:to_s)
+        #   only   = options && options[:only]   && Array.wrap(options[:only]).map(&:to_s)
+
+        #   self.class.uploaders.each do |column, uploader|
+        #     if (!only && !except) || (only && only.include?(column.to_s)) || (except && !except.include?(column.to_s))
+        #       hash[column.to_s] = _mounter(column.to_sym).uploader.serializable_hash
+        #     end
+        #   end
+        #   super(options).merge(hash)
+        # end
+
+        def serializable_hash(options = nil)
+          super(options).tap do |result_hash|
+            self.class.uploaders.each do |column, _|
+              if result_hash.has_key?(column.to_s) || result_hash.has_key?(column)
+                serialized_key = result_hash.has_key?(column.to_s) ? column.to_s : column
+                column_uploaders = _mounter(column.to_sym).uploaders
+                result_hash[serialized_key] = if column_uploaders.count > 1
+                  column_uploaders.map(&:serializable_hash)
+                else
+                  if column_uploader = column_uploaders.first
+                    column_uploader.serializable_hash
+                  else
+                    {}
+                  end
+                end
+              end
+            end
+          end
+        end
+
+      RUBY
+    end
+
   end # Mongoid
 end # CarrierWave
 
+    
 CarrierWave::Storage.autoload :GridFS, 'carrierwave/storage/grid_fs'
 
 class CarrierWave::Uploader::Base
---
+++ spec/multiple_image_upload.rb
@@ -0,0 +1,84 @@
+# encoding: utf-8
+require 'spec_helper'
+require 'byebug'
+require 'carrierwave/mongoid'
+
+def reset_mongo_class(uploader = MongoUploader)
+  define_mongo_class('MongoUser') do
+    include Mongoid::Document
+    store_in :collection => 'users'
+    field :folder, :default => ''
+    mount_uploaders :images, uploader
+  end
+end
+
+def define_mongo_class(class_name, &block)
+  Object.send(:remove_const, class_name) rescue nil
+  klass = Object.const_set(class_name, Class.new)
+  klass.class_eval(&block)
+  klass
+end
+
+class MongoUploader < CarrierWave::Uploader::Base; end
+class AnotherMongoUploader < CarrierWave::Uploader::Base; end
+
+class IntegrityErrorUploader < CarrierWave::Uploader::Base
+  process :monkey
+  def monkey
+    raise CarrierWave::IntegrityError
+  end
+  def extension_white_list
+    %w(jpg)
+  end
+end
+
+class ProcessingErrorUploader < CarrierWave::Uploader::Base
+  process :monkey
+  def monkey
+    raise CarrierWave::ProcessingError
+  end
+  def extension_white_list
+    %w(jpg)
+  end
+end
+
+
+describe CarrierWave::Mongoid do
+
+  after do
+    MongoUser.collection.drop if MongoUser.count > 0
+  end
+
+  describe '#images' do
+
+    context "when nothing is assigned" do
+
+      before do
+        mongo_user_klass = reset_mongo_class
+        @document = mongo_user_klass.new
+      end
+
+      it "returns an empty array" do
+        @document.images.should be_blank
+      end
+
+    end
+
+    context "when an empty string is assigned" do
+
+      before do
+        mongo_user_klass = reset_mongo_class
+        @document = mongo_user_klass.new(:images => [])
+        @document.save
+      end
+
+      it "returns an empty array" do
+        @saved_doc = MongoUser.first
+        @saved_doc.images.should be_blank
+      end
+
+    end
+
+  end
+
+end
