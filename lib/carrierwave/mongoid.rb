364f512041d42d592f41ad34c312793fe0ae35ff
Leo Arnold <larnold@givve.com>
2020-12-03 20:30:49 +0100

    Implement 'mount_uploaders' (plural). Closes #138
    CarrierWave offers the plural `mount_uploaders` since v1.0.
    When used with `carrierwave-mongoid` this resulted in
    a silent fail, as it lacked the implementation on the model.
    
    This commit adapts all unit tests from singular
    to the plural case and adds the missing implementation.

--- lib/carrierwave/mongoid.rb
+++ lib/carrierwave/mongoid.rb
@@ -143,6 +143,96 @@ module CarrierWave
         end
       RUBY
     end
+
+    if Gem::Version.new(CarrierWave::VERSION) >= Gem::Version.new('1.0.beta')
+      def mount_uploaders(column, uploader = nil, options = {}, &block)
+        field (options[:mount_on] || column), type: Array, default: []
+
+        super
+
+        alias_method :read_uploader, :read_attribute
+        alias_method :write_uploader, :write_attribute
+        public :read_uploader
+        public :write_uploader
+
+        include CarrierWave::Validations::ActiveModel
+
+        validates_integrity_of column if uploader_option(column.to_sym, :validate_integrity)
+        validates_processing_of column if uploader_option(column.to_sym, :validate_processing)
+
+        before_update :"store_previous_changes_for_#{column}"
+        before_save :"write_#{column}_identifier"
+        after_save :"store_#{column}!"
+        after_save :"remove_previously_stored_#{column}"
+        after_destroy :"remove_#{column}!"
+
+        class_eval <<-RUBY, __FILE__, (__LINE__ + 1)
+          def #{column}=(new_files)
+            column = _mounter(:#{column}).serialization_column
+  
+            previous_uploader_value = read_uploader(column)
+            @_previous_uploader_value_for_#{column} = previous_uploader_value
+  
+            write_uploader(column, []) if self.persisted? && read_uploader(column).nil?
+  
+            send(:"\#{column}_will_change!")
+
+            super
+          end
+
+          def #{column}_changed?
+            changed_attributes.has_key?("#{column}")
+          end
+
+          def remove_#{column}=(value)
+            if ['1', true].include?(value)
+              column = _mounter(:#{column}).serialization_column
+
+              send(:"\#{column}_will_change!")
+            end
+
+            super
+          end
+
+          # The default Mongoid attribute_will_change! method is not enough
+          # when we want to upload a new file in an existing embedded document.
+          # The custom version of that method forces the callbacks to be
+          # ran and so does the upload.
+          def #{column}_will_change!
+            changed_attributes["#{column}"] = ['_new_']
+          end
+
+          def remove_previously_stored_#{column}
+            before, after = @_previous_changes_for_#{column}
+            # Don't delete if the files had the same name
+            return if before.nil? && after.nil?
+            # Proceed to remove the file, use the original name instead of '_new_'
+            before = @_previous_uploader_value_for_#{column} || before
+            _mounter(:#{column}).remove_previous(Array.wrap(before), Array.wrap(after))
+          end
+
+          def serializable_hash(options = nil)
+            hash = {}
+  
+            except = options && options[:except] && Array.wrap(options[:except]).map(&:to_s)
+            only = options && options[:only] && Array.wrap(options[:only]).map(&:to_s)
+  
+            self.class.uploaders.each do |column, _uploader|
+              if (!only && !except) || (only && only.include?(column.to_s)) || (except && !except.include?(column.to_s))
+                next if _mounter(column.to_sym).uploaders.blank?
+                hash[column.to_s] = _mounter(column.to_sym).uploaders.map(&:serializable_hash)
+              end
+            end
+
+            super(options).merge(hash)
+          end
+
+          def store_previous_changes_for_#{column}
+            @_previous_changes_for_#{column} = changes[_mounter(:#{column}).serialization_column]
+          end
+        RUBY
+      end
+    end
   end # Mongoid
 end # CarrierWave
 
